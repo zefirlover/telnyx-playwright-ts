bd24d0fc7ad01f0351013701bfd06bee04127eb8
zefirlover <ibodikmushta@gmail.com>
2022-06-15 11:01:01 +0300

    Trying to start in headless mode

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,6 +33,7 @@ const config: PlaywrightTestConfig = {
   reporter: "allure-playwright",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    headless: true,
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
