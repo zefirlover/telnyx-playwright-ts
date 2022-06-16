fe7fa85771aae950389c011c30e289e98c6a7bcf
zefirlover <ibodikmushta@gmail.com>
2022-06-16 16:13:08 +0300

    Cut the firefox and webkit

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ const config: PlaywrightTestConfig = {
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
+    },/*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
@@ -28,7 +28,7 @@ const config: PlaywrightTestConfig = {
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
-    },
+    },*/
   ],
 };
 
