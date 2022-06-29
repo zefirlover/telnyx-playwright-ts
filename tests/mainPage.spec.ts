eced4335dc521e12ffcf54b5bb32828b5bb459dd
zefirlover <ibodikmushta@gmail.com>
2022-06-29 14:10:23 +0300

    Optimize TNP-03-10, 42 and TNP-11-16, 41

--- tests/mainPage.spec.ts
+++ tests/mainPage.spec.ts
@@ -2,17 +2,13 @@ import { test, expect } from '@playwright/test';
 import { MainPage } from '../pages/mainpage/Main.page';
 import { SignUpPage } from '../../telnyx-playwright-ts/pages/SignUp.page';
 import Helpers from '../helpers/helper';
-const arrProducts1 = [
+const arrProducts = [
     { id: 0, link: 'sms-api', checkText: 'Send and receive texts' },
-    { id: 1, link: 'video', checkText: 'integrated video solution' }
-];
-const arrProducts2 = [
-    { id: 0, link: 'iot-sim-card', checkText: 'IoT SIM Cards' },
-    { id: 1, link: 'voice-api', checkText: 'Voice API' }
-];
-const arrProducts3 = [
-    { id: 0, link: 'sip-trunks', checkText: 'SIP Trunks' },
-    { id: 1, link: 'storage', checkText: 'cloud storage' }
+    { id: 1, link: 'video', checkText: 'integrated video solution' },
+    { id: 2, link: 'iot-sim-card', checkText: 'IoT SIM Cards' },
+    { id: 3, link: 'voice-api', checkText: 'Voice API' },
+    { id: 4, link: 'sip-trunks', checkText: 'SIP Trunks' },
+    { id: 5, link: 'storage', checkText: 'cloud storage' }
 ];
 const arrIncorrectEmails = [ 'testAtgmail.com', '@gmail' ]
 
@@ -30,38 +26,11 @@ test.describe('main page testing', () => {
         await expect(mainPage.emailConfirmForm).toBeVisible();
     })
 
-    test(`TNP-03-10,42 part 1 Verify the 'Powerful products' cards table and it cards`, async ({ page }) => {
+    test(`TNP-03-10,42 Verify the 'Powerful products' cards table and it cards`, async ({ page }) => {
         let mainPage = new MainPage(page);
-        for (let i = 0; i < arrProducts1.length; i++) {
-            let element = arrProducts1.find(e => e.id === i);
-            let cardLink = page.locator(`main *> a[href*="/${element?.link}"]`);
-            let headerString = `//h1//strong[contains(text(), '${element?.checkText}')]|//h1//span[contains(text(), '${element?.checkText}')]`;
-            let headerText = page.locator(headerString);
-            await mainPage.scrollToPowerfulProducts();
-            await cardLink.click();
-            await expect(headerText).toBeVisible();
-            await mainPage.visit();
-        }
-    })
-
-    test(`TNP-03-10,42 part 2 Verify the 'Powerful products' cards table and it cards`, async ({ page }) => {
-        let mainPage = new MainPage(page);
-        for (let i = 0; i < arrProducts2.length; i++) {
-            let element = arrProducts2.find(e => e.id === i);
-            let cardLink = page.locator(`main *> a[href*="/${element?.link}"]`);
-            let headerString = `//h1//strong[contains(text(), '${element?.checkText}')]|//h1//span[contains(text(), '${element?.checkText}')]`;
-            let headerText = page.locator(headerString);
-            await mainPage.scrollToPowerfulProducts();
-            await cardLink.click();
-            await expect(headerText).toBeVisible();
-            await mainPage.visit();
-        }
-    })
-
-    test(`TNP-03-10,42 part 3 Verify the 'Powerful products' cards table and it cards`, async ({ page }) => {
-        let mainPage = new MainPage(page);
-        for (let i = 0; i < arrProducts3.length; i++) {
-            let element = arrProducts3.find(e => e.id === i);
+        test.setTimeout(120000);
+        for (let i = 0; i < arrProducts.length; i++) {
+            let element = arrProducts.find(e => e.id === i);
             let cardLink = page.locator(`main *> a[href*="/${element?.link}"]`);
             let headerString = `//h1//strong[contains(text(), '${element?.checkText}')]|//h1//span[contains(text(), '${element?.checkText}')]`;
             let headerText = page.locator(headerString);
